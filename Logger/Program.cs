d17878371b06fddce9548c0d1fa64c0ec556a9e0
test <k.widlak@microsoft.wsei.edu.pl>
2019-01-29 19:58:22 +0100

    zmianki

--- Logger/Program.cs
+++ Logger/Program.cs
@@ -10,33 +10,16 @@ namespace Logger
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < args.Length; i++)
+            FileLogger.Logger logger = new FileLogger.Logger("Info.txt");
+
+            while (true)
             {
-                Console.WriteLine(args[i]);
+                Console.WriteLine("Wpisz coś: ");
+                var text = Console.ReadLine();
+                logger.Log(text);
+                Console.Read();
             }
 
-            Console.WriteLine("---------------------");
-
-            foreach (var item in args)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine("---------------------");
-
-            List<string> temp = new List<string>();
-            temp.AddRange(args);
-            //foreach (var item in args)
-            //{
-            //    temp.Add(item);
-            //}
-
-            while (temp.Count > 0)
-            {
-                Console.WriteLine(temp[0]);
-                temp.Remove(temp[0]);
-            }
-            Console.Read();
         }
     }
 }
