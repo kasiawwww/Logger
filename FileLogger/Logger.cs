879cda61558bdc0da6d62fe5c384d51a09dfeca4
test <k.widlak@microsoft.wsei.edu.pl>
2019-01-29 18:39:38 +0100

    różne rzeczy

---
+++ FileLogger/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLogger
+{
+    public class Logger
+    {
+        private readonly string filename; //stała a readonly -> conts musi miec od razu wartosc przypisaną, a readonly raz ale w dowolnej metodzie
+
+
+        public Logger() //ctr tab tab ->tworzenie konstruktora
+        {
+                
+        }
+
+        public Logger(string filename) //ctrl + . -> tworzenie properties / fields
+        {
+            this.filename = filename;
+        }
+    }
+}
--- Logger/Program.cs
+++ Logger/Program.cs
@@ -10,7 +10,33 @@ namespace Logger
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
 
+            Console.WriteLine("---------------------");
+
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("---------------------");
+
+            List<string> temp = new List<string>();
+            temp.AddRange(args);
+            //foreach (var item in args)
+            //{
+            //    temp.Add(item);
+            //}
+
+            while (temp.Count > 0)
+            {
+                Console.WriteLine(temp[0]);
+                temp.Remove(temp[0]);
+            }
+            Console.Read();
         }
     }
 }
